a1e8467c7c131bb407d582f40c47d1e80862f0fb
suraj <suraj@192.168.0.102>
2018-06-27 19:11:41 +0530

    Custom login page done

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -12,9 +12,9 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("john").password("secret123").roles("EMPLOYEE");
-		auth.inMemoryAuthentication().withUser("mary").password("secret123").roles("MANAGER");
-		auth.inMemoryAuthentication().withUser("susan").password("secret123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("john").password("test123").roles("EMPLOYEE");
+		auth.inMemoryAuthentication().withUser("mary").password("test123").roles("MANAGER");
+		auth.inMemoryAuthentication().withUser("susan").password("test123").roles("ADMIN");
 	}
 
 	@Override
